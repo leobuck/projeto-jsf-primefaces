e8e73b424c088b705c81b471a5bbbf2bbadef0a7
leobuck <leobuck12@gmail.com>
2021-06-16 20:53:46 -0300

    .

--- src/br/com/frameworks/utils/UtilFramework.java
+++ src/br/com/frameworks/utils/UtilFramework.java
@@ -1,5 +1,17 @@
 package br.com.frameworks.utils;
 
-public class UtilFramework {
+import java.io.Serializable;
 
+import org.springframework.stereotype.Component;
+
+@Component
+public class UtilFramework implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
+	
+	public synchronized static ThreadLocal<Long> getThreadLocal() {
+		return threadLocal;
+	}
 }
