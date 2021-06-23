5a3af52847e500990972c5a28a5174a4c1178ade
leobuck <leobuck12@gmail.com>
2021-06-23 19:07:56 -0300

    Criando a classe ObjetoCampoConsulta

--- src/br/com/project/annotation/IdentificaCampoPesquisa.java
+++ src/br/com/project/annotation/IdentificaCampoPesquisa.java
@@ -1,5 +1,17 @@
 package br.com.project.annotation;
 
-public @interface IdentificaCampoPesquisa {
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Target(value = ElementType.FIELD)
+@Retention(value = RetentionPolicy.RUNTIME)
+@Documented
+public abstract @interface IdentificaCampoPesquisa {
+
+	String descricaoCampo();
+	String campoConsulta();
+	int principal() default 10000;
 }
