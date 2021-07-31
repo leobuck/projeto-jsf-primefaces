595b8d9d8aa84d2f6338c745403fe1a7a9389f3e
leobuck <leobuck12@gmail.com>
2021-07-30 22:31:34 -0300

    Criada função de Alterar Senha.

--- src/br/com/project/bean/view/EntidadeBeanView.java
+++ src/br/com/project/bean/view/EntidadeBeanView.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 
 import br.com.framework.interfac.crud.InterfaceCrud;
 import br.com.project.bean.geral.BeanManagedViewAbstract;
+import br.com.project.bean.geral.EntidadeAtualizaSenhaBean;
 import br.com.project.geral.controller.EntidadeController;
 import br.com.project.model.classes.Entidade;
 
@@ -25,6 +26,8 @@ public class EntidadeBeanView extends BeanManagedViewAbstract {
 
 	@Autowired
 	private EntidadeController entidadeController;
+	
+	private EntidadeAtualizaSenhaBean entidadeAtualizaSenhaBean = new EntidadeAtualizaSenhaBean();
 
 	public String getUsuarioLogadoSecurity() {
 		return contextoBean.getAuthentication().getName();
@@ -34,6 +37,30 @@ public class EntidadeBeanView extends BeanManagedViewAbstract {
 		return contextoBean.getEntidadeLogada().getEntUltimoAcesso();
 	}
 
+	public void updateSenha() throws Exception {
+		Entidade entidadeLogada = contextoBean.getEntidadeLogada();
+		if (!entidadeAtualizaSenhaBean.getSenhaAtual().equals(entidadeLogada.getEntSenha())) {
+			addMsg("Senha atual digitada inválida!");
+			return;
+		} else if (entidadeAtualizaSenhaBean.getSenhaAtual().equals(entidadeAtualizaSenhaBean.getNovaSenha())) {
+			addMsg("Nova senha não pode ser igual a senha atual!");
+			return;
+		} else if (!entidadeAtualizaSenhaBean.getNovaSenha().equals(entidadeAtualizaSenhaBean.getConfirmaSenha())) {
+			addMsg("Nova senha digitada diferente da confirmação!");
+			return;
+		} else {
+			entidadeLogada.setEntSenha(entidadeAtualizaSenhaBean.getNovaSenha());
+			entidadeController.saveOrUpdate(entidadeLogada);
+			entidadeLogada = entidadeController.findById(Entidade.class, entidadeLogada.getEntCodigo());
+			if (entidadeLogada.getEntSenha().equals(entidadeAtualizaSenhaBean.getNovaSenha())) {
+				sucesso();
+			} else {
+				addMsg("Nova senha não atualizada!");
+				erro();
+			}
+		}
+	}
+	
 	@Override
 	protected Class<Entidade> getClassImplement() {
 		return Entidade.class;
@@ -48,4 +75,13 @@ public class EntidadeBeanView extends BeanManagedViewAbstract {
 	public String condicaoAndParaPesquisa() throws Exception {
 		return "";
 	}
+
+	public EntidadeAtualizaSenhaBean getEntidadeAtualizaSenhaBean() {
+		return entidadeAtualizaSenhaBean;
+	}
+
+	public void setEntidadeAtualizaSenhaBean(EntidadeAtualizaSenhaBean entidadeAtualizaSenhaBean) {
+		this.entidadeAtualizaSenhaBean = entidadeAtualizaSenhaBean;
+	}
+	
 }
